5d9c5ee2a93c8b903a44031f9e88a335b989819f
dindakece <37007113+dindakece@users.noreply.github.com>
2018-03-03 09:58:03 +0700

    Add files via upload

--- safelink.js
+++ safelink.js
@@ -47,7 +47,7 @@ KODE IKLAN DISINI
 <b>Your link show here</b> <i aria-hidden="true" class="fa fa-hand-o-down"></i></h3>
 </div>
 <div class="panel-body">
-<script src="https://cdn.rawgit.com/dedi96/safelink/813ce72f/safelinkga.js" type="text/javascript"></script>
+<script src="https://cdn.rawgit.com/dindakece/safelink/99891b39/safelink.js" type="text/javascript"></script>
 <script type="text/javascript">
 var currentURL=location.href;
 var str = currentURL;
